d5e33c06064b1771b30d79cd1a4130318d753dd0
Nathaniel Meadows <str8upjayhawk10@gmail.com>
2020-11-12 15:38:46 -0800

    added font awesome link, created carousel added sport imgs

--- Assets/Sports-info/sports.js
+++ Assets/Sports-info/sports.js
@@ -1,22 +1,40 @@
-        console.log(dataNews.articles[i])
-        
-        for(var i = 0; i < dataNews.articles.length; i++){
-            console.log(dataNews.articles[i])
-            let a = document.createElement('a')
-            a.setAttribute('href', dataNews.articles[i].url);
-            a.setAttribute('target', '_blank');
-            a.innerHTML = dataNews.articles[i].url;
-            
-            let li = document.createElement('li');
-            let img = document.createElement('img');
-            img.src = dataNews.articles[i].urlToImage
-            li.appendChild(a)
-            li.appendChild(img);
-            ul.appendChild(li)
-            
-}
-}).catch((error)=> {
-    console.log(error)
-})
+// const searchFrom = document.querySelector('.search'); 
+// const input = document.querySelector("#search-terms");
+// const submit = document.querySelector("#submit");
+// const newsList = document.querySelector(".news-list");
 
-    })
+// searchFrom.addEventListener("submit", retrieve);
+
+// function retrieve(e) {
+// e.preventDefault();
+// console.log("retrieve");
+// if (input.value == "") {
+//     alert("Please enter a topic!");
+//     return;
+// }
+
+
+// const apiKey = '03t2vzx89s51l3pm63er9rpxw'
+// let topic = input.value;
+// newsList.innerHTML = "";
+// fetch(`http://api.datanews.io/v1/headlines?apiKey=${apiKey}&language=en&q=${topic}`)
+// .then(res => res.json()).then(dataNews => {
+// console.log(dataNews)
+
+// for (var i = 0; i < dataNews.hits.length; i++) {
+//         console.log(dataNews.hits[i]);
+//         let li = document.createElement("li");
+//         let a = document.createElement("a");
+//         a.setAttribute("href", dataNews.hits[i].url);
+//         a.setAttribute("target", "_blank");
+//         a.innerHTML = dataNews.hits[i].title;
+//         li.appendChild(a);
+//         newsList.appendChild(li);
+// }
+// })
+// .catch((error) => {
+// console.log(error);
+//     });
+// }
+
+
