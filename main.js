1c9453ba784e4bbc4dec901af99c5fc85bc21e86
Japkirat Singh <japkirats@gmail.com>
2020-11-12 18:50:14 -0800

    making some style changes

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const searchFrom = document.querySelector('.search'); 
+const searchFrom = document.querySelector(".search");
 const input = document.querySelector("#search-terms");
 const submit = document.querySelector("#submit");
 const newsList = document.querySelector(".news-list");
@@ -6,22 +6,24 @@ const newsList = document.querySelector(".news-list");
 searchFrom.addEventListener("submit", retrieve);
 
 function retrieve(e) {
-e.preventDefault();
-console.log("retrieve");
-if (input.value == "") {
+  e.preventDefault();
+  console.log("retrieve");
+  if (input.value == "") {
     alert("Please enter a topic!");
     return;
-}
+  }
 
+  const apiKey = "03t2vzx89s51l3pm63er9rpxw";
+  let topic = input.value;
+  newsList.innerHTML = "";
+  fetch(
+    `http://api.datanews.io/v1/headlines?apiKey=${apiKey}&language=en&q=${topic}`
+  )
+    .then((res) => res.json())
+    .then((dataNews) => {
+      console.log(dataNews);
 
-const apiKey = '03t2vzx89s51l3pm63er9rpxw'
-let topic = input.value;
-newsList.innerHTML = "";
-fetch(`http://api.datanews.io/v1/headlines?apiKey=${apiKey}&language=en&q=${topic}`)
-.then(res => res.json()).then(dataNews => {
-console.log(dataNews)
-
-for (var i = 0; i < dataNews.hits.length; i++) {
+      for (var i = 0; i < dataNews.hits.length; i++) {
         console.log(dataNews.hits[i]);
         let li = document.createElement("li");
         let a = document.createElement("a");
@@ -30,11 +32,9 @@ for (var i = 0; i < dataNews.hits.length; i++) {
         a.innerHTML = dataNews.hits[i].title;
         li.appendChild(a);
         newsList.appendChild(li);
-}
-})
-.catch((error) => {
-console.log(error);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
     });
 }
-
-
